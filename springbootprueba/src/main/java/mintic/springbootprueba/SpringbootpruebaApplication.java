5d446b89b46df070e1ce065e1ae05f5dea7ed900
ANGELLO PERILLA <angello.perilla.mt@correo.usa.edu.co>
2021-10-27 09:58:29 -0500

    Spring Boot Ejemlo

---
+++ springbootprueba/src/main/java/mintic/springbootprueba/SpringbootpruebaApplication.java
@@ -0,0 +1,13 @@
+package mintic.springbootprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpruebaApplication.class, args);
+	}
+
+}
---
+++ springbootprueba/src/test/java/mintic/springbootprueba/SpringbootpruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.springbootprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
